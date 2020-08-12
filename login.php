f738cd288c24a537dc544bfc06c7af2a61ff6234
adri711 <35468047+adri711@users.noreply.github.com>
2020-08-12 09:39:41 +0100

    Update login.php

--- login.php
+++ login.php
@@ -7,30 +7,32 @@
 		exit();
 	}
 	require('header.php');
-	echo "<div class='box'>";
-	echo "<h2>M711 - Login</h2>";
-	echo "<p>In order to play the game you need to have an account, login to your account or <a href='register.php'>register here</a>.</p>";
-	echo "<form method='post'>";
-	echo "<table>
-			<tr>
-				<td><p for=usernameinput>Username:</label></td>
-				<td>
-					<input placeholder='Enter your username'type='text' name='usernameinput' required>
-				</td>
-			</tr>
-			<tr>
-				<td><p for=passwordinput>Password:</p></td>
-				<td><input placeholder='Enter your password' type='password' name='passwordinput' required></td>
-			</tr>
-			<tr>
-				<td colspan=2>
-					<input style='margin:10px; background-color:orange' type='submit' value='Click to login' name=loginsubmit>
-				</td>
-			</tr>
-		</table>";
-	echo "</form>";
-	echo "</div>";
+?>
 
+<div class='box'>
+<h2>M711 - Login</h2>
+<p>In order to play the game you need to have an account, login to your account or <a href='register.php'>register here</a>.</p>
+<form method='post'>
+<table>
+		<tr>
+			<td><p for=usernameinput>Username:</label></td>
+			<td>
+				<input placeholder='Enter your username'type='text' name='usernameinput' required>
+			</td>
+		</tr>
+		<tr>
+			<td><p for=passwordinput>Password:</p></td>
+			<td><input placeholder='Enter your password' type='password' name='passwordinput' required></td>
+		</tr>
+		<tr>
+		<td colspan=2>
+			<input style='margin:10px; background-color:orange' type='submit' value='Click to login' name=loginsubmit>
+		</td>
+	</tr>
+</table>
+</form>
+</div>
+<?php
 	if(isset($_POST['loginsubmit'])) {
 		$row = $db->query("SELECT * FROM users WHERE username = '$_POST[usernameinput]' AND password = '$_POST[passwordinput]' LIMIT 1");
 		if(mysqli_num_rows($row) < 1) {
